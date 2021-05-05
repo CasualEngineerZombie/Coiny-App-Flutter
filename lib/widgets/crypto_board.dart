f42341cc033dfb39b3981c515337d891e13db392
CodePhilanthropist <barrigaete@gmail.com>
2021-05-05 22:11:07 +0800

    removed .0

--- lib/widgets/crypto_board.dart
+++ lib/widgets/crypto_board.dart
@@ -195,7 +195,7 @@ class CryptoBoardDetail extends StatelessWidget {
                       color: bottomBorderColor.withOpacity(0.1),
                     ),
                     bottom: BorderSide(
-                      width: 2.0,
+                      width: 2,
                       color: bottomBorderColor.withOpacity(0.1),
                     ),
                   ),
